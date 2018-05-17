9b8bd9cacb367ff38d5d5fb5a264ab98f73f05b2
harbargerdev <aharbarger@gmail.com>
2018-05-17 14:46:37 -0400

    Removing unnecessary references

--- PhotoStoragePOC.DocumentUpload/S3/DocumentUploadUtility.cs
+++ PhotoStoragePOC.DocumentUpload/S3/DocumentUploadUtility.cs
@@ -1,13 +1,7 @@
-﻿using Amazon.Runtime;
-using Amazon.S3;
+﻿using Amazon.S3;
 using Amazon.S3.Model;
 using PhotoStoragePOC.DocumentUpload.Entities;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PhotoStoragePOC.DocumentUpload.S3
 {
