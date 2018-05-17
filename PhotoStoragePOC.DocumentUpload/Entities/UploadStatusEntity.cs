bde15b8f169a3f10bb4d9b9045f4cd1644451545
harbargerdev <aharbarger@gmail.com>
2018-05-17 11:55:07 -0400

    Document Upload base state

---
+++ PhotoStoragePOC.DocumentUpload/Entities/UploadStatusEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoStoragePOC.DocumentUpload.Entities
+{
+    public class UploadStatusEntity
+    {
+        public bool IsSuccess { get; set; } 
+        public string ErrorMessage { get; set; }
+        public string DocumentUrl { get; set; }
+
+        public UploadStatusEntity()
+        {
+            IsSuccess = false;
+            ErrorMessage = string.Empty;
+            DocumentUrl = string.Empty;
+        }
+    }
+}
--- PhotoStoragePOC.DocumentUpload/S3/DocumentUploadUtility.cs
+++ PhotoStoragePOC.DocumentUpload/S3/DocumentUploadUtility.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Model;
+using PhotoStoragePOC.DocumentUpload.Entities;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,15 +17,51 @@ namespace PhotoStoragePOC.DocumentUpload.S3
         private string SecretKey;
         private string DefaultRegion;
         private string BucketName;
+
+        private static AmazonS3Client s3Client;
         
         public DocumentUploadUtility()
         {
         }
 
+        /// <summary>
+        /// Constructor to take parameters for the Key and Secret for AWS.
+        /// </summary>
+        /// <param name="key">AWS Key</param>
+        /// <param name="secret">AWS Secret</param>
         public DocumentUploadUtility(string key, string secret)
         {
             AccessKey = key;
             SecretKey = secret;
+        }
+
+        /// <summary>
+        /// Constructor that takes the bucket, key, and secret
+        /// </summary>
+        /// <param name="bucketName">AWS Bucket name</param>
+        /// <param name="key">AWS Key</param>
+        /// <param name="secret">AWS Secret</param>
+        public DocumentUploadUtility(string bucketName, string key, string secret)
+        {
+            BucketName = bucketName;
+            AccessKey = key;
+            SecretKey = secret;
+        }
+
+        public UploadStatusEntity UploadDocumentToS3(FileStream stream, string fileName, string userId)
+        {
+            UploadStatusEntity uploadStatus = new UploadStatusEntity();
+
+            s3Client = new AmazonS3Client(AccessKey, SecretKey, DefaultRegion);
+
+            PutObjectRequest request = new PutObjectRequest()
+            {
+                BucketName = BucketName,
+                Key = userId + '/' + fileName,
+                InputStream = stream
+            };
+
+            return uploadStatus;
         }
     }
 }
