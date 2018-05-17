f12a3cfd787265f4f98265ab2a90b1fff8f242da
harbargerdev <aharbarger@gmail.com>
2018-05-16 21:48:46 -0400

    Restarting the project, starting small then will scale once I get it working on cmdline

---
+++ PhotoStoragePOC.DocumentUpload/Entities/DocumentEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoStoragePOC.DocumentUpload.Entities
+{
+    public class DocumentEntity
+    {
+        public long ID { get; set; }
+        public string DocumentOwner { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime LastUpdateDate { get; set; }
+        public string FileName { get; set; }
+        public string Url { get; set; }
+        public string DocumentType { get; set; }
+
+        public DocumentEntity()
+        {
+        }
+    }
+}
---
+++ PhotoStoragePOC.DocumentUpload/S3/DocumentUploadUtility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoStoragePOC.DocumentUpload.S3
+{
+    public class DocumentUploadUtility
+    {
+        private string AccessKey;
+        private string SecretKey;
+        private string DefaultRegion;
+        private string BucketName;
+        
+        public DocumentUploadUtility()
+        {
+        }
+
+        public DocumentUploadUtility(string key, string secret)
+        {
+            AccessKey = key;
+            SecretKey = secret;
+        }
+    }
+}
