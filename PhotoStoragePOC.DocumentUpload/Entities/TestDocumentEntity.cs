bbd19396e7a23a798bc2ab29c3ad409e6bfd12c7
harbargerdev <aharbarger@gmail.com>
2018-05-23 21:13:54 -0400

    TestDocument For Console

---
+++ PhotoStoragePOC.DocumentUpload/Entities/TestDocumentEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoStoragePOC.DocumentUpload.Entities
+{
+    public class TestDocumentEntity
+    {
+        public string FileName { get; set; }
+        public string Owner { get; set; }
+        public string Contents { get; set; }
+        public string Url { get; set; }
+    }
+}
